fc02800965edaae78e3069a5c4a2dc22c2d04543
Caleb Einarr <earthctzn1@gmail.com>
2019-04-03 04:30:34 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while
-      while levitation_force = 6
-  num_of_hotdogs_eaten += 1
+      while levitation_force < 10 
+  levitation_force += 1
   puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
 end
 
