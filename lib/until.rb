68c8cd28ad33be119334bbab077c648fcb878f55
Caleb Einarr <earthctzn1@gmail.com>
2019-04-03 04:25:21 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
         levitation_force = 6
-        #your code here
+while levitation_force < 10
+  puts "The current number is less than 20."
+  counter += 1
+end#your code here
     
 end
 
